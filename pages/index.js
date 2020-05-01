2401ba1ec636429d34c6d5aab368fe7422fed608
Lorenzo Cinque <lore.cinque@gmail.com>
2020-05-01 20:16:20 +0100

    Add more sounds

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,11 @@ export default () => (
     <SoundsGrid>
       <SoundWidget title="Restaurant" path="/sounds/restaurant.wav" />
       <SoundWidget title="Fireplace" path="/sounds/fireplace.wav" />
+      <SoundWidget title="Heavy Raing" path="/sounds/heavy-rain.wav" />
+      <SoundWidget title="Ocean Wave" path="/sounds/ocean-wave.wav" />
+      <SoundWidget title="Water Churning" path="/sounds/water-churning.wav" />
+      <SoundWidget title="Steam Train" path="/sounds/steam-train.wav" />
+      <SoundWidget title="Thunder" path="/sounds/thunder.wav" />
     </SoundsGrid>
   </Layout>
 );
